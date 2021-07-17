00151a0511856561c94590c91abfe2095d7162ad
NWylynko <nick@wylynko.com>
2021-07-17 13:47:11 +0000

    does it auto deploy?

--- src/endpoints/getMessages.ts
+++ src/endpoints/getMessages.ts
@@ -1,7 +1,7 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export const getMessages = async () => {
-  return { hello: "world" };
+  return { hello: "world 2" };
 };
 
 export const getMessagesHandler = async (
